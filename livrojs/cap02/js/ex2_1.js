1919ee4caf6a2d34faaf2665442a1321805c75bf
mateus silva <mateus.s.sousa13@aluno.senai.br>
2025-08-27 21:46:15 -0300

    20250827: livro cap 2.4

---
+++ livrojs/cap02/js/ex2_1.js
@@ -0,0 +1,11 @@
+//criar referencia ao form e ao elemento h3 (onde sera exibido a resposta)
+const frm = document.querySelector("form")
+const resp = document.querySelector("h3")
+
+//criar um "ouvinte" de evento, acionado quando o botao submit for clicado
+frm.addEventListener("submit", (e) => {
+    const nome = frm.inNome.value //obter o nome digitado no formulario
+    resp.innerHTML = `olá <i>${nome}</i>`//exibir a resposta do programa
+    e.preventDefault() //evitar o envio do formulario
+
+}) 
