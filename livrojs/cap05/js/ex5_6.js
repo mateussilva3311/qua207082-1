df784d2e605e06787e31f59a6f9716401b15e673
mateus silva <mateus.s.sousa13@aluno.senai.br>
2025-09-09 21:41:37 -0300

    2025 09 09: estrelas

--- livrojs/cap05/js/ex5_6.js
+++ livrojs/cap05/js/ex5_6.js
@@ -4,15 +4,17 @@ const resp1 = document.querySelector("h3")
 frm.addEventListener("submit", (e) => {
     e.preventDefault()
     const numero = Number(frm.inNumero.value)
-    let numeroDivisires = 0// declara e inicializa o contador
-    for (let i = 1; i <= numero; i++) {
-        if (numero % i == 0) {
-            numeroDivisires++
+    let primo = true// declara e inicializa o contador
+    //PERCORRE OS POSIVEIS DIVISORES IMPARES INICIANDO DO 3 ATE METADE DO NUMERO
+    for (let i = 3; i <= numero/2 ; i = i + 2 ) {
+        if (numero % i == 0) {//VERIFICA SE (3,5,7...) É DIVISOR DO NuMERO
+            primo = false // mudar flag
+            break//sair do loop
         }
     }
-    if(numeroDivisires == 2){
+    if((numero<=3 || numero%2!=0)&& primo){
         resp1.innerText = `${numero} é primo`
     }else{
-        resp1.innerText = `${numero} não é primo`
+        resp1.innerText = `${numero} NÃO é primo`
     }
 })
---
+++ livrojs/cap05/js/ex5_7.js
@@ -0,0 +1,18 @@
+const frm = document.querySelector("form")
+const resp = document.querySelector("h3")
+
+frm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const numero = Number(frm.inNumero.value)
+    let estrelas = ""// variavel que ira concatenar as estreslas/traços
+
+    for(let i = 1; i<= numero; i++){
+        if (i%2==1) {
+            estrelas+='*'
+            
+        }else{
+            estrelas +='-'
+        }
+    }
+    resp.innerText=estrelas
+})
