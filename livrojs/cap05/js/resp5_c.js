0bb5d777358355d2eb45951ad49f360a58b0abe0
mateus silva <mateus.s.sousa13@aluno.senai.br>
2025-09-12 21:46:16 -0300

    aula 2025 09 13: alteração nas respostas

--- livrojs/cap05/js/resp5_c.js
+++ livrojs/cap05/js/resp5_c.js
@@ -5,21 +5,20 @@ const resp2 = document.querySelector("h4")
 frm.addEventListener("submit", (e) => {
     e.preventDefault()
     const numero = Number(frm.inNumero.value)
-    let divisores = ""
-    let soma = 0
+    let divisores = `Divisores de ${numero}: 1`
+    let soma = 1
 
-    for(let i = 1; i <= numero/2; i++){
+    for(let i = 2; i <= numero/2; i++){
         if(numero % i == 0){
             soma += i
-            divisores +=`${i},`
+            divisores += ',' + i
         }
 
     }
+    resp1.innerText = `${divisores} (soma: ${soma})`
     if(soma == numero){
-        resp1.innerText = `Divisore de ${numero}: ${divisores} (soma: ${soma})`
         resp2.innerText = `${numero} É um numero perfeito`
     }else{
-        resp1.innerText = `Divisore de ${numero}: ${divisores} (soma: ${soma})`
         resp2.innerText = `${numero} NÃO é um numero perfeito`
     }
    
