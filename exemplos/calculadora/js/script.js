e74f988f6e520983c8ba0f9b4f941252b6f41b35
mateus silva <mateus.s.sousa13@aluno.senai.br>
2025-10-02 21:23:19 -0300

    aula 2025 10 02: calculadora finalizada

--- exemplos/calculadora/js/script.js
+++ exemplos/calculadora/js/script.js
@@ -24,3 +24,9 @@ function calcular() {
         visor.value = "Erro"
     }
 }
+
+function porcentagem() {
+    calcular();
+    const visor = document.querySelector("#visor");
+    visor.value = visor.value / 100;
+}
