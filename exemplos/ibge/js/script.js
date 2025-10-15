0302935555230bd7ae11e980b062a483778869a6
mateus silva <mateus.s.sousa13@aluno.senai.br>
2025-10-14 21:28:08 -0300

    aula 20251010: bootstrap

--- exemplos/ibge/js/script.js
+++ exemplos/ibge/js/script.js
@@ -1,8 +1,28 @@
 const frm = document.querySelector('form')
 
+
 async function getData(url) {
-    const resposta = await fetch(url)
+    const resposta = await fetch("https://servicodados.ibge.gov.br/api/v1/localidades/" + url)
     const data = await resposta.json()
     return data
 }
+function carreguarselect(select, url) {
+    getData(url).then((ls) => {
+        // console.table(ls)
+        for (const i of ls) {
+            const option = document.createElement("option")
+            option.value = i.id
+            option.innerText = `${i.nome} (${i.sigla})`
+            select.appendChild(option)
+        }
+    })
+}
 
+carreguarselect(frm.inRegiao, "regioes")
+carreguarselect(frm.inEstado, "estados")
+
+frm.inEstado.addEventListener("change",()=>{
+    const uf = frm.inEstado.value
+    frm.inMunicipio.innerHTML = ""
+    carreguarselect(frm.inMunicipio, `estados/${uf}/municipios`)
+})
