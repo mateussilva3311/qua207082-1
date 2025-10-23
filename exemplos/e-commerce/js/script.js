35de436766706619cf7d3adf5c0070bf938571ca
mateus silva <mateus.s.sousa13@aluno.senai.br>
2025-10-22 21:21:06 -0300

    aula 2025 10 22: começo do js

---
+++ exemplos/e-commerce/js/script.js
@@ -0,0 +1,7 @@
+const original = document.querySelector(".produto")
+const produtos = document.querySelector("#produtos")
+produtos.innerHTML = ""
+for(let i = 0; i < 9; i++){
+const clone = original.cloneNode(true)
+produtos.appendChild(clone)
+}
