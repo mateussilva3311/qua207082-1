af64da9d2ca0ff517ec0fdee9d4e39de127f32ef
mateus silva <mateus.s.sousa13@aluno.senai.br>
2025-10-22 21:43:45 -0300

    aula 2025 10 22: script pra mudar os produtos altomaticamente

---
+++ exemplos/e-commerce/js/produtos.js
@@ -0,0 +1,52 @@
+const lsProduto = [
+    {
+        nome: "Banho de mar",
+        descricao: "vela aromatica - 300g",
+        img: "img/vela-01.png",
+        valor: 39
+    },
+    {
+        nome: "paseio na floresta",
+        descricao: "vela aromatica - 300g",
+        img: "img/vela-01.png",
+        valor: 39
+    },
+    {
+        nome: "trio bom dia",
+        descricao: "vela aromatica - 300g",
+        img: "img/vela-01.png",
+        valor: 39
+    },
+    {
+        nome: "meus cristais",
+        descricao: "vela aromatica - 300g",
+        img: "img/vela-01.png",
+        valor: 39
+    },
+    {
+        nome: "campo de levada",
+        descricao: "vela aromatica - 300g",
+        img: "img/vela-01.png",
+        valor: 39
+    },
+    {   nome: "minha rotina",
+        descricao: "vela aromatica - 300g",
+        img: "img/vela-01.png",
+        valor: 39
+    },
+    {   nome: "vida no campo",
+        descricao: "vela aromatica - 300g",
+        img: "img/vela-01.png",
+        valor: 39
+    },
+    {   nome: "meu mundo",
+        descricao: "vela aromatica - 300g",
+        img: "img/vela-01.png",
+        valor: 39
+    },
+    {   nome: "floresta vivas",
+        descricao: "vela aromatica - 300g",
+        img: "img/vela-01.png",
+        valor: 39
+    },
+]
