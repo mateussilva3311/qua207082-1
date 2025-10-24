3536b9b20f60e19c3019f862bc30546c72f6c62d
mateus silva <mateus.s.sousa13@aluno.senai.br>
2025-10-23 21:50:36 -0300

    aula 2025 10 23: diferança de fundo selecionar carrinho com qt selecionado

--- exemplos/e-commerce/js/produtos.js
+++ exemplos/e-commerce/js/produtos.js
@@ -8,45 +8,45 @@ const lsProduto = [
     {
         nome: "paseio na floresta",
         descricao: "vela aromatica - 300g",
-        img: "img/vela-01.png",
+        img: "img/vela-02.png",
         valor: 39
     },
     {
         nome: "trio bom dia",
         descricao: "vela aromatica - 300g",
-        img: "img/vela-01.png",
+        img: "img/vela-03.png",
         valor: 39
     },
     {
         nome: "meus cristais",
         descricao: "vela aromatica - 300g",
-        img: "img/vela-01.png",
+        img: "img/vela-04.png",
         valor: 39
     },
     {
         nome: "campo de levada",
         descricao: "vela aromatica - 300g",
-        img: "img/vela-01.png",
+        img: "img/vela-05.png",
         valor: 39
     },
     {   nome: "minha rotina",
         descricao: "vela aromatica - 300g",
-        img: "img/vela-01.png",
+        img: "img/vela-06.png",
         valor: 39
     },
     {   nome: "vida no campo",
         descricao: "vela aromatica - 300g",
-        img: "img/vela-01.png",
+        img: "img/vela-07.png",
         valor: 39
     },
     {   nome: "meu mundo",
         descricao: "vela aromatica - 300g",
-        img: "img/vela-01.png",
+        img: "img/vela-08.png",
         valor: 39
     },
     {   nome: "floresta vivas",
         descricao: "vela aromatica - 300g",
-        img: "img/vela-01.png",
+        img: "img/vela-09.png",
         valor: 39
     },
 ]
--- exemplos/e-commerce/js/script.js
+++ exemplos/e-commerce/js/script.js
@@ -5,7 +5,37 @@ for (const p of lsProduto) {
     const clone = original.cloneNode(true)
     clone.querySelector(".nome").innerText = p.nome
     clone.querySelector(".descricao").innerText = p.descricao
-    clone.querySelector("img").scr = p.img
+    clone.querySelector("img").src = p.img
     clone.querySelector(".valor").innerText = `R$ ${p.valor.toFixed(2)}`
     produtos.appendChild(clone)
 }
+document.querySelectorAll(".produto").forEach((p,i)=>{
+    p.addEventListener("click",()=>{
+        p.classList.toggle("marcado")
+        if(lsProduto[i].marcado == undefined){
+            lsProduto[i].marcado = 1
+        }else{
+            delete lsProduto[i].marcado
+        }
+        atualizarQT()
+    })
+})
+
+function atualizarQT(){
+    const qt= lsProduto.filter(p => p.marcado == 1)
+    document.querySelector("#qt").innerText = qt.length > 0 ? qt.length : ""
+
+    // if(qt.length > 0){
+    //     document.querySelector("#qt").style.display = "block"
+    // }else{
+    //     document.querySelector("#qt").style.display = "none"
+    // }
+}
+
+document.querySelector("#btVela").addEventListener("click",carrinho)
+
+function carrinho(){
+    const qt = document.querySelectorAll("#qt").innerText
+    if (qt == "") {
+        alert("necessario selecionar 1 item")
+    }}
