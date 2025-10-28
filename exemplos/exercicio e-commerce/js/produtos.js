3d05e866764552cf0ab5334c59324b4c36da401f
mateus silva <mateus.s.sousa13@aluno.senai.br>
2025-10-27 21:46:49 -0300

    aula 2025 10 27: execicio e-commerci

---
+++ exemplos/exercicio e-commerce/js/produtos.js
@@ -0,0 +1,56 @@
+const lsProduto = [
+  {
+    nome: "Corrida Urbana",
+    descricao: "tênis esportivo - leve e respirável",
+    img: "img/tenis-01.png",
+    valor: 199
+  },
+  {
+    nome: "Estilo Street",
+    descricao: "tênis casual - design moderno",
+    img: "img/tenis-02.png",
+    valor: 179
+  },
+  {
+    nome: "Pista Livre",
+    descricao: "tênis esportivo - sola antiderrapante",
+    img: "img/tenis-03.png",
+    valor: 229
+  },
+  {
+    nome: "Caminho Suave",
+    descricao: "tênis casual - conforto diário",
+    img: "img/tenis-04.png",
+    valor: 159
+  },
+  {
+    nome: "Trail Master",
+    descricao: "tênis esportivo - ideal para trilhas",
+    img: "img/tenis-05.png",
+    valor: 249
+  },
+  {
+    nome: "Vibe Minimal",
+    descricao: "tênis casual - estilo minimalista",
+    img: "img/tenis-06.png",
+    valor: 169
+  },
+  {
+    nome: "Power Run",
+    descricao: "tênis esportivo - performance e conforto",
+    img: "img/tenis-07.png",
+    valor: 239
+  },
+  {
+    nome: "Day Off",
+    descricao: "tênis casual - ideal para o dia a dia",
+    img: "img/tenis-08.png",
+    valor: 189
+  },
+  {
+    nome: "Flex Sport",
+    descricao: "tênis esportivo - tecido flexível",
+    img: "img/tenis-09.png",
+    valor: 219
+  }
+]
